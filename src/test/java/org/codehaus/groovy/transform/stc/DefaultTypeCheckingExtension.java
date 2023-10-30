5d92b49e54c79c3cabf9b6d373ffc529763c7712
naperdish <pihuihush@1hokage.ru>
2023-10-30 16:39:04 +0300

    ubeitemenya

---
+++ src/test/java/org/codehaus/groovy/transform/stc/DefaultTypeCheckingExtension.java
@@ -0,0 +1,8 @@
+package org.codehaus.groovy.transform.stc;
+
+public enum DefaultTypeCheckingExtension {
+    ;
+
+    public void setup() {
+    }
+}
---
+++ src/test/java/ru/netology/webselenium/WebDriverManager.java
@@ -0,0 +1,7 @@
+package ru.netology.webselenium;
+
+public class WebDriverManager {
+    public static org.codehaus.groovy.transform.stc.DefaultTypeCheckingExtension chromedriver() {
+        return null;
+    }
+}
